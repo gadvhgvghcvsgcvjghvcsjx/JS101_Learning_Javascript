9b2edeea23c2f3a4857ec7c481b56822253d715c
unknown <subhamsingh87654321@gmail.com>
2024-01-07 15:15:23 +0530

    index2 added

---
+++ index2.js
@@ -0,0 +1,13 @@
+// Problem1
+console.log("Masai School")
+
+// Problem2
+console.log(number*50)
+
+// Problem3
+
+console.log(one*two*three*four*five*six)
+
+// Problem4
+console.log(one+two+three+four+five)
+
